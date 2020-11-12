d0375e011dc1eab9e2d77bedbacc81306281028f
atain050 <207410290@gms.tku.edu.tw>
2020-11-12 15:14:34 +0800

    edit member components

--- client/src/components/member.jsx
+++ client/src/components/member.jsx
@@ -9,13 +9,22 @@ const Member = () => {
                 <Profilepicture />
             </Backgroundicture>
             <List>
-                pigone efkfkf @Pigone55 x_x
-                {/* <Bio type="text"></Bio> */}
+                <p>pigone efkfkf</p>
+                <p>@Pigone55 </p>
+                <p>x_x</p>
             </List>
             <List2>
-                <About href="">About</About>
-                <About href="">About</About>
-                <About href="">About</About>
+                <Listul>
+                    <Listli>
+                        <About href="">關於</About>
+                    </Listli>
+                    <Listli>
+                        <About href="">揪團</About>
+                    </Listli>
+                    <Listli>
+                        <About href="">參與</About>
+                    </Listli>
+                </Listul>
             </List2>
         </Container>
     );
@@ -24,15 +33,26 @@ const Member = () => {
 export default Member;
 
 const Container = styled.div`
-    max-width: 1140px;
+    width: 100%;
     background-color: #ffffff;
 `;
 
 const Backgroundicture = styled.div`
-    max-width: 1140px;
     background-color: rgba(130, 119, 119, 0.1);
-    padding-top: 45px;
+    padding-top: 65px;
     padding-bottom: 10px;
+
+    background: #dae2f8; /* fallback for old browsers */
+    background: -webkit-linear-gradient(
+        to right,
+        #dae2f8,
+        #d6a4a4
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(
+        to right,
+        #dae2f8,
+        #d6a4a4
+    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 `;
 
 const Profilepicture = styled.div`
@@ -41,7 +61,6 @@ const Profilepicture = styled.div`
     border-radius: 50%;
     background-color: rgba(0, 0, 0, 0.08);
     margin-left: 25px;
-    display: flex;
 `;
 
 const List = styled.div`
@@ -49,18 +68,23 @@ const List = styled.div`
     padding-left: 30px;
     height: 85px;
     border-top: solid rgba(0, 0, 0, 0.2) 1px;
-    font-size: inherit;
+    font-size: 16px;
 `;
 
-// const Bio = styled.input``;
-
 const List2 = styled.div`
+    font-size: 16px;
+`;
+
+const Listul = styled.ul`
     background-color: #ffffff;
     border-top: solid rgba(0, 0, 0, 0.2) 1px;
-    font-size: inherit;
+    display: flex;
+`;
+
+const Listli = styled.li`
+    padding-left: 30px;
 `;
 
 const About = styled.a`
-    padding: 0px 15px 0px 30px;
     color: black;
 `;
